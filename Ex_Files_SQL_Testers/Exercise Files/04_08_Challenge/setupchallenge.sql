5bc5fa7dad1b9d0d020cc9e0c2c1670c3451476b
Shuvankar Dash <shuvankard32@gmail.com>
2025-08-13 11:33:31 +0200

    commiting new files

---
+++ Ex_Files_SQL_Testers/Exercise Files/04_08_Challenge/setupchallenge.sql
@@ -0,0 +1,37 @@
+INSERT INTO classes
+VALUES
+(1,"Math"),
+(2,"Language"),
+(3,"Science"),
+(4, "Art");
+
+INSERT INTO roles
+VALUES
+(1, "Student"),
+(2, "Teacher"),
+(3, "Admin");
+
+INSERT INTO users
+VALUES
+(1, 'Rosemary', 'Enriquez', 'renriquez0', 'jQKS3frCf'),
+(2, 'Ellen', 'Abby', 'eabby1', 'GFt9cJwb'),
+(3, 'Enrichetta', 'Horstead', 'ehorstead2', 'LWCVgf'),
+(4, 'Randie', 'Hele', 'rhele3', '1gz9qEPI'),
+(5, 'Anabella', 'Mayers', 'amayers4', 'wXvz9oqRD5p'),
+(6, 'Corrina', 'Wildsmith', 'cwildsmith5', 'r6DFjAC'),
+(7, 'Shea', 'Popworth', 'spopworth6', 'Ji4l9Od3LY'),
+(8, 'Blondelle', 'Bredee', 'bbredee7', 'kmrQNtvB68l'),
+(9, 'Jeffrey', 'Revelle', 'jrevelle8', 'Injilzqi2kxZ'),
+(10, 'Madelaine', 'Aldwick', 'maldwick9', 'iCOWaF7Zsqnl');
+
+INSERT INTO enrollments
+VALUES
+(1,1,1),
+(1,1,3),
+(1,2,2),
+(2,1,4),
+(2,1,5),
+(2,2,2),
+(3,1,6),
+(3,1,7),
+(3,2,8);
